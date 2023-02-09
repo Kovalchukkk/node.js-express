c012b0ac6eeba95f26a93e8ebfdb816ac9c570cf
Mykola Kovalchuk <kolyankov@gmail.com>
2023-02-09 12:32:12 +0200

    update

--- index.js
+++ index.js
@@ -3,8 +3,16 @@ const express = require("express");
 const PORT = 5000;
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
+  // console.log(req.query.page);
   res.status(200).json("Some useful information");
+});
+
+app.post("/users", (req, res) => {
+  console.log(req.body);
+  res.status(200).json("User is created");
 });
 
 app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`));
